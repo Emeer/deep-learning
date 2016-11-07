7c57adcf00e7d348065a447d1312e31120406bd5
Jia ZHOU <jia.zhou@orchestra.eu>
2016-11-07 17:02:07 +0100

    Update hadoop et spark

---
+++ src/main/java/com/eztech/deep/learning/Application.java
@@ -0,0 +1,24 @@
+package com.eztech.deep.learning;
+
+import com.eztech.deep.learning.config.HadoopConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Spring Big data Application
+ *
+ * @author Jia ZHOU
+ */
+@SpringBootApplication
+@Import(HadoopConfig.class)
+public class Application {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
