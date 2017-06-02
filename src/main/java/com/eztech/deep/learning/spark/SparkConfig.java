143c625f4ca230e6df72636522cd6d4fdf773feb
Jia ZHOU <jia.zhou@orchestra.eu>
2017-06-02 18:08:48 +0200

    add spark config

---
+++ src/main/java/com/eztech/deep/learning/spark/SparkConfig.java
@@ -0,0 +1,41 @@
+package com.eztech.deep.learning.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jia on 02/06/2017.
+ */
+@Configuration
+public class SparkConfig {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${spark.home}")
+    private String sparkHome;
+
+    @Value("${master.uri}")
+    private String masterUri;
+
+
+    @Bean
+    public SparkConf sparkConf() {
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setAppName(appName);
+        sparkConf.setSparkHome(sparkHome);
+        sparkConf.setMaster(masterUri);
+        return sparkConf;
+    }
+
+
+    @Bean
+    public JavaSparkContext javaSparkContext() {
+        return new JavaSparkContext(sparkConf());
+    }
+
+}
